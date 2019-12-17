b5fb11f404ce1bfaab1767377246a54bff245803
potato-prog <53585256+potato-prog@users.noreply.github.com>
2019-12-16 19:13:13 -0600

    project init

---
+++ src/main/java/potato/cleavefile/CleaveFileApplication.java
@@ -0,0 +1,13 @@
+package potato.cleavefile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleaveFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleaveFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/potato/cleavefile/CleaveFileApplicationTests.java
@@ -0,0 +1,13 @@
+package potato.cleavefile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleaveFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
