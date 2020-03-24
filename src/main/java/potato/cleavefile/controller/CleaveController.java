d73dffc03f7dea59f06a96f6dbf8bba3a4c1fd57
potato-prog <53585256+potato-prog@users.noreply.github.com>
2020-03-24 18:59:07 -0500

    update....

--- src/main/java/potato/cleavefile/controller/CleaveController.java
+++ src/main/java/potato/cleavefile/controller/CleaveController.java
@@ -1,84 +1,84 @@
-package potato.cleavefile.controller;
-
-import org.apache.logging.log4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-@Component
-@Controller
-public class CleaveController {
-
-    private Logger log = (Logger) LoggerFactory.getLogger("log");
-
-    @Value("$(file.dir)")
-    private String fileDirPathLocation;
-
-    private String inputDir = "input";
-    private String outputDir = "output";
-    private String processDir = "processing";
-    private String archiveDir = "archived";
-    private String errorDir = "error";
-
-    @Scheduled(cron = "0 */2 * * * *")
-    public void processor() throws IOException {
-        for(String fileSourcePathStr : getFiles()) {
-            processFile(fileSourcePathStr);
-        }
-    }
-
-    private void processFile(String fileSourcePathStr) throws IOException {
-
-        Path sourcePath = Paths.get(fileSourcePathStr);
-        String filename = sourcePath.getFileName().toString();
-        boolean isSuccess = false;
-
-        log.info("File {} cleave initiated :: ", filename );
-
-        Path processingFilePath = moveFile(inputDir, processDir, filename);
-
-        BufferedWriter writer = createBufferedFileWriter(filename);
-        try (BufferedReader reader = new BufferedReader(new FileReader(processingFilePath.toFile()))) {
-            String readLine = "";
-            while ((readLine = reader.readLine()) != null) {
-                if (readLine.startsWith("SPLIT;")) {
-                    closeWriter(writer);
-                    renameOutputFileName(filename, readLine.substring(readLine.lastIndexOf('/') + 1));
-
-                    readLine = reader.readLine();
-                    if (readLine != null) {
-                        writer = createBufferedFileWriter(filename);
-                        writeBufferedLine(writer, readLine);
-                    }
-                } else {
-                    writeBufferedLine(writer, readLine);
-                }
-            }
-            isSuccess = true;
-            log.info(" File {} cleaving process is completed successfully", filename);
-        } catch (IOException e) {
-            moveFile(processDir, errorDir, filename);
-            deleteIncompleteOutputFile(filename);
-            log.error("Error found in file {} processing", filename, e);
-        } finally {
-            if (isSuccess) {
-                moveFile(processDir, archiveDir, filename);
-                log.info("File {} is archived successfully", filename);
-            }
-        }
-    }
-
-
-
-    
-
-}
+//package potato.cleavefile.controller;
+//
+//import org.apache.logging.log4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Controller;
+//
+//import java.io.BufferedReader;
+//import java.io.BufferedWriter;
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//
+//@Component
+//@Controller
+//public class CleaveController {
+//
+//    private Logger log = (Logger) LoggerFactory.getLogger("log");
+//
+//    @Value("$(file.dir)")
+//    private String fileDirPathLocation;
+//
+//    private String inputDir = "input";
+//    private String outputDir = "output";
+//    private String processDir = "processing";
+//    private String archiveDir = "archived";
+//    private String errorDir = "error";
+//
+//    @Scheduled(cron = "0 */2 * * * *")
+//    public void processor() throws IOException {
+//        for(String fileSourcePathStr : getFiles()) {
+//            processFile(fileSourcePathStr);
+//        }
+//    }
+//
+//    private void processFile(String fileSourcePathStr) throws IOException {
+//
+//        Path sourcePath = Paths.get(fileSourcePathStr);
+//        String filename = sourcePath.getFileName().toString();
+//        boolean isSuccess = false;
+//
+//        log.info("File {} cleave initiated :: ", filename );
+//
+//        Path processingFilePath = moveFile(inputDir, processDir, filename);
+//
+//        BufferedWriter writer = createBufferedFileWriter(filename);
+//        try (BufferedReader reader = new BufferedReader(new FileReader(processingFilePath.toFile()))) {
+//            String readLine = "";
+//            while ((readLine = reader.readLine()) != null) {
+//                if (readLine.startsWith("SPLIT;")) {
+//                    closeWriter(writer);
+//                    renameOutputFileName(filename, readLine.substring(readLine.lastIndexOf('/') + 1));
+//
+//                    readLine = reader.readLine();
+//                    if (readLine != null) {
+//                        writer = createBufferedFileWriter(filename);
+//                        writeBufferedLine(writer, readLine);
+//                    }
+//                } else {
+//                    writeBufferedLine(writer, readLine);
+//                }
+//            }
+//            isSuccess = true;
+//            log.info(" File {} cleaving process is completed successfully", filename);
+//        } catch (IOException e) {
+//            moveFile(processDir, errorDir, filename);
+//            deleteIncompleteOutputFile(filename);
+//            log.error("Error found in file {} processing", filename, e);
+//        } finally {
+//            if (isSuccess) {
+//                moveFile(processDir, archiveDir, filename);
+//                log.info("File {} is archived successfully", filename);
+//            }
+//        }
+//    }
+//
+//
+//
+//
+//
+//}
---
+++ src/main/java/potato/cleavefile/controller/ListFilesUtil.java
@@ -0,0 +1,60 @@
+package potato.cleavefile.controller;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class ListFilesUtil {
+
+    public Set<String> listFilesUsingFileWalk(String dir, int depth) throws IOException {
+        try (Stream<Path> stream = Files.walk(Paths.get(dir), depth)) {
+            return stream
+                    .filter(file -> !Files.isDirectory(file))
+                    .map(Path::getFileName)
+                    .map(Path::toString)
+                    .collect(Collectors.toSet());
+        }
+    }
+
+
+
+    public Set<String> listFilesUsingDirectoryStream(String dir) throws IOException {
+        Set<String> fileList = new HashSet<>();
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(dir))) {
+            for (Path path : stream) {
+                if (!Files.isDirectory(path)) {
+                    fileList.add(path.getFileName()
+                            .toString());
+                }
+            }
+        }
+        return fileList;
+    }
+
+    public Set<String> listFilesUsingJavaIO(String dir) {
+        return Stream.of(new File(dir).listFiles())
+                .filter(file -> !file.isDirectory())
+                .map(File::getName)
+                .collect(Collectors.toSet());
+    }
+
+
+    public static void main(String[] args) {
+
+        File file = new File("C:\\Users");
+        String[] files = file.list();
+        for (String s : files) {
+            System.out.println(s);
+        }
+
+    }
+}
